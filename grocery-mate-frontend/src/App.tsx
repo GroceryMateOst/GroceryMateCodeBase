5fa3296d5ec8be954c1ee2c28a423152e1ffc533
glv12 <gianluca.vogel@ost.ch>
2023-02-27 21:06:37 +0100

    Add frontend, backend and connect it together

---
+++ grocery-mate-frontend/src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { ClickService } from './services/click-serbice'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const countUp = async () =>{
+    const clickService = new ClickService();
+    const newCount = count+1
+    await clickService.sendClicks(newCount);
+    setCount(newCount)
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={countUp}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
---
+++ grocery-mate-frontend/src/services/click-serbice.ts
@@ -0,0 +1,11 @@
+﻿import { AxiosBaseService } from "./axios-base.service";
+
+export class ClickService extends AxiosBaseService{
+    constructor(){
+        super('/Click');
+    }
+
+    public async sendClicks(number: number): Promise<any>{
+        return await this.instance.post(`?number=${number}`).then(this.responseBody)
+    }
+}
