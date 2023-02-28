f9c9988c3e97e65413bfe623416fa714413575f8
glv12 <gianluca.vogel@ost.ch>
2023-02-28 10:35:15 +0100

    Add efcore to backend

--- grocery-mate-backend/Controllers/ClickController.cs
+++ grocery-mate-backend/Controllers/ClickController.cs
@@ -1,6 +1,8 @@
 ﻿using System.Net;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using grocery_mate_backend.Data;
+using grocery_mate_backend.Models;
 
 namespace grocery_mate_backend.Controllers;
 
@@ -8,11 +10,45 @@ namespace grocery_mate_backend.Controllers;
 [Route("[controller]")]
 public class ClickController : ControllerBase
 {
+    private readonly ClickDbContext _context;
+    private readonly ILogger<ClickController> _logger;
+
+    public ClickController(ILogger<ClickController> logger, ClickDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
     [HttpPost(Name = "SendClick")]
-    public HttpResponseMessage SendNumber(int number)
+    public async Task<IActionResult> SendNumber(int number)
     {
         Console.WriteLine(number);
-        return new HttpResponseMessage(HttpStatusCode.OK);
+        var newOrExisting = _context.Clicks.Find(1);
+        if(newOrExisting == null){
+        _context.Add(new Click{
+            id = 1,
+            click = number,
+        });
+        }else{
+            newOrExisting.click = number;
+        }
+        await _context.SaveChangesAsync();
+
+        return Ok();
     }
-    
+
+    [HttpGet(Name = "GetClicks")]
+    public IActionResult GetNumber()
+    {
+        var click = _context.Clicks.Find(1);
+        if(click == null){
+            var newClick = new Click{
+                id = 1,
+                click = 0,
+            };
+            return Ok(newClick);
+        }
+        return Ok(click);
+    }
+
 }
--- grocery-mate-backend/Program.cs
+++ grocery-mate-backend/Program.cs
@@ -1,6 +1,12 @@
+using grocery_mate_backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var conn = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<ClickDbContext>(options => options.UseNpgsql(conn));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
