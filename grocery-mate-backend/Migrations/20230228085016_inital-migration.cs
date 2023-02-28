8134b427f3815698d22ddf54e2326ec2ab41280b
glv12 <gianluca.vogel@ost.ch>
2023-02-28 10:35:41 +0100

    Add forgoten files

---
+++ grocery-mate-backend/Migrations/20230228085016_inital-migration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace grocery_mate_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class initalmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clicks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    click = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clicks", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clicks");
+        }
+    }
+}
---
+++ grocery-mate-backend/Models/Click.cs
@@ -0,0 +1,7 @@
+﻿namespace grocery_mate_backend.Models;
+
+public class Click
+{
+    public int id { get; set; }
+    public int click { get; set; }
+}
