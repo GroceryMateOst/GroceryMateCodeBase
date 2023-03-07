06177f3e8d4a0f9b53aedb58bea6b029a1398f3b
Marc Kissling <20038674+mOceanracing@users.noreply.github.com>
2023-03-07 09:37:27 +0100

    #36 Add Test-Project and Collaboration-Examples

---
+++ backend/grocery-mate-backend-UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace grocery_mate_backend_UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
