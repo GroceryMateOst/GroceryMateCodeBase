2c4412601a1fb4348055dfbb83d5b6895261fa28
Marc Kissling <20038674+mOceanracing@users.noreply.github.com>
2023-03-16 09:21:25 +0100

    add UnitTest example-file [#43]

---
+++ backend/grocery-mate-backend/Sandbox/UnitExample.cs
@@ -0,0 +1,20 @@
+﻿
+namespace grocery_mate_backend.Sandbox;
+
+public class UnitExample
+{
+    public User MadeBy{ get; set;} = new User();
+
+    public bool CanBeCanceled(User user)
+    {
+        if (user.IsAdmin  || MadeBy == user )
+            return true;
+        return MadeBy == user;
+    }
+}
+
+public class User
+{
+    public bool IsAdmin { get; set; }
+    public bool IsMale { get; set; }
+}
