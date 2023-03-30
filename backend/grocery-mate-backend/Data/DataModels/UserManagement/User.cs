862f7c80409c015f396f80e7be70d63bb4874a1e
tranfelix <tranfelix99@gmail.com>
2023-03-30 14:22:01 +0200

    Add db migrations

--- backend/grocery-mate-backend/Data/DataModels/UserManagement/User.cs
+++ backend/grocery-mate-backend/Data/DataModels/UserManagement/User.cs
@@ -19,7 +19,7 @@ public class User
     public string Password { get; set; }
     public IdentityUser? Identity { get; set; }
     
-    public Guid AddressId { get; set; }
+    public Guid? AddressId { get; set; }
 
     public User(CreateUserUserDto dto)
     {
