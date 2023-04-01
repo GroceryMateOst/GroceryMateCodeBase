5911e5c1a56abeaba6931c59cf8498d122ac4dd1
Ramon Ebneter <ramon.ebneter@bluewin.ch>
2023-04-01 10:41:25 +0200

    change deplyoment settings

--- backend/grocery-mate-backend/Controllers/WeatherForecastController.cs
+++ backend/grocery-mate-backend/Controllers/WeatherForecastController.cs
@@ -19,7 +19,6 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
     
-    [Authorize]
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
