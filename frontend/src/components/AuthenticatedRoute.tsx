b69c80d031fff983100096c07e5583bbf3c0fa9d
glv12 <gianluca.vogel@ost.ch>
2023-04-27 16:52:13 +0200

    Add frogoten file[#87]

---
+++ frontend/src/components/AuthenticatedRoute.tsx
@@ -0,0 +1,24 @@
+﻿import { Route, useNavigate } from 'react-router-dom';
+import { useAppSelector } from '../redux/hooks';
+import HomePage from '../pages/HomePage';
+
+interface AuthenticatedRouteProps {
+	element: React.ReactNode;
+	redirectElement?: React.ReactNode;
+}
+
+const AuthenticatedRoute = ({
+	element,
+	redirectElement,
+}: AuthenticatedRouteProps) => {
+	const isAuthenticated = useAppSelector((state) => state.user.isAuthenticated);
+	return isAuthenticated ? (
+		<>{element}</>
+	) : redirectElement ? (
+		<>{redirectElement}</>
+	) : (
+		<HomePage />
+	);
+};
+
+export default AuthenticatedRoute;
