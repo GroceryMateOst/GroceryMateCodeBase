edeaa7113c531004270b1d24d3d728a267034c9d
glv12 <gianluca.vogel@ost.ch>
2023-05-05 09:49:37 +0200

    Add forgoten file

---
+++ frontend/src/pages/SearchPage.tsx
@@ -0,0 +1,42 @@
+﻿import { SearchOutlined } from '@ant-design/icons';
+import { Button, Input, Space } from 'antd';
+import { useEffect, useState } from 'react';
+import { GroceryRequestResponseModel } from '../models/GroceryRequestModel';
+import ShoppingService from '../services/shopping-service';
+
+const SearchPage = () => {
+	const [plz, setPlz] = useState<number>();
+	const [groceryRequests, setgroceryRequests] = useState<
+		GroceryRequestResponseModel[]
+	>([]);
+
+	const onSearchClick = () => {
+		console.log(plz);
+	};
+
+	useEffect(() => {}, []);
+
+	const getFilteredRequests = async () => {
+		const shoppingService = new ShoppingService();
+	};
+
+	return (
+		<div className="px-20 mb-10">
+			<h1>Suche</h1>
+			<p>Bitte gib die Postleitzahl ein, um die Einkaufslisten zu filtern.</p>
+			<Space.Compact>
+				<Input
+					type="number"
+					onBlur={(e) =>
+						setPlz(e.target.value ? parseInt(e.target.value) : undefined)
+					}
+				/>
+				<Button onClick={onSearchClick} disabled={plz === undefined}>
+					<SearchOutlined />
+				</Button>
+			</Space.Compact>
+		</div>
+	);
+};
+
+export default SearchPage;
